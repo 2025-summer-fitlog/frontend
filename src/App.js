8fbf33ed6bad7e3bc8ab46b75e5d455736b8763f
dmlwls <dmlwls04061@gmail.com>
2025-08-12 16:16:31 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import ExerciseRecord from './components/ExerciseRecord';
 import MemoInput from './components/MemoInput';
 import FeedbackForm from './components/FeedbackForm';
-import LogPage from './log/LogPage';
+// import LogPage from './log/LogPage';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -9,7 +9,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* 기본 루트("/")로 왔을 때 보여줄 컴포넌트들 */}
         <Route
           path="/"
           element={
@@ -21,8 +20,6 @@ function App() {
           }
         />
 
-        {/* log 탭을 눌렀을 때 보여줄 페이지 */}
-        <Route path="/log" element={<LogPage />} />
       </Routes>
     </Router>
   );
