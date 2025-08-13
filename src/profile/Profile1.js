70c1c5005dc96c56814f6dd64c86fdd62b193f70
seoyoon <heu0303@naver.com>
2025-08-13 17:25:51 +0900

    withCredentials

--- src/profile/Profile1.js
+++ src/profile/Profile1.js
@@ -46,6 +46,7 @@ function Profile1() {
 
         try {
             const response = await fetch("https://fitlog-2025.duckdns.org/api/profile/personal-info", {
+                withCredentials: true,
                 method: "PUT",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(body)
